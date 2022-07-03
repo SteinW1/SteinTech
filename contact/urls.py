68346c9c1c4a40b21823aeecea16b9931896797e
SteinW1 <will.stein@yahoo.com>
2022-07-03 16:50:33 -0700

    Updated contacts app.

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import ContactFormView
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact-form'),
+    path('contact/', ContactFormView.as_view(), name='contact-form'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,69 +1,36 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
 from django.conf import settings
+from django.views.generic import FormView
 from .forms import ContactForm
-import requests # import requests module for easier http for google recaptcha API
+from requests import post as send_post_request 
 
-def contact(request):
-    
-    context = {
-            'recaptcha_site_key': settings.RECAPTCHA_SITE_KEY,
-        }
-        
-    if request.method =="POST":
-        form = ContactForm(request.POST)
-        context['form'] = form # add form to context dictionary after it is declared
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'contact/contact.html'
+    success_url = '/contact/' 
 
-        if form.is_valid():
-            
-            reCAPTCHA_validation_result = validateRecaptcha(request)
-            
-            if reCAPTCHA_validation_result['success']:
-                from_name = form.cleaned_data['from_name']
-                subject = form.cleaned_data['subject']
-                from_email = form.cleaned_data['from_email']
-                contact_message = form.cleaned_data['message']
-                
-                try:
-                    send_mail(subject, contact_message, from_email, ['admin@williamstein.com'])
-                except BadHeaderError:
-                    messages.error(request, f'Invalid header found.')
-                    return redirect('contact-form')
-                
-                messages.success(request, f'Thank you, your message has been sent!')
-                return redirect('contact-form')
-            else:
-                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
-                
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(recaptcha_site_key = settings.RECAPTCHA_SITE_KEY, **kwargs)
+
+    def form_valid(self, form):
+        if validateRecaptcha(self.request):                              
+            try:
+                send_mail(
+                    form.cleaned_data['subject'],
+                    form.cleaned_data['message'],
+                    form.cleaned_data['from_email'],
+                    ['admin@williamstein.com'],
+                    )
+            except BadHeaderError:
+                messages.error(self.request, f'Invalid header found.')
+            messages.success(self.request, f'Thank you, your message has been sent!')
         else:
-            for error_message in form.errors:
-                messages.error(request, f'{form.errors[error_message]}')
-                
-    else:
-        form = ContactForm()
-        context['form'] = form # add form to context dictionary after it is declared
-    return render(request, 'contact/contact.html', context)
-    
+            messages.error(self.request, 'Invalid reCAPTCHA. Please try again.')
+        return super().form_valid(form)
 
-def validateRecaptcha(request):
-    ''' 
-    Function for validating a user with google's reCAPTCHA.
-    
-    Args:
-        request: http request
-    Returns:
-        validated recaptcha result in format {'success': bool, 'challenge_ts': str, 'hostname': str, 'score': int 'action': str}
-    '''
-    recaptcha_response = request.POST.get('g-recaptcha-response')
-    
-    data = {
-        'secret': settings.RECAPTCHA_SECRET_KEY,
-        'response': recaptcha_response,
-        }
-    
-    r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-    reCAPTCHA_result = r.json()
-    
-    return reCAPTCHA_result
+def validateRecaptcha(request) -> bool:
+    '''Function for validating a POST request with google's reCAPTCHA.'''    
+    data = {'secret': settings.RECAPTCHA_SECRET_KEY, 'response': request.POST.get('g-recaptcha-response')}
+    reCAPTCHA_result = send_post_request('https://www.google.com/recaptcha/api/siteverify', data=data).json()
+    return reCAPTCHA_result['success']
