ec882d057ed300ebe31e6ba9fb628c9fc4e110a3
SteinW1 <will.stein@yahoo.com>
2022-07-08 19:05:55 -0700

    Refactored RecipCreatView to be more intuative.

--- recipes/validators.py
+++ recipes/validators.py
@@ -11,6 +11,5 @@ def validate_unit_of_measurement(test_unit:str):
         raise ValidationError(f'Something went wrong. Try again or cantact admin if necessary.')
 
 def validate_quantity_measurement(amount:str):
-    print(number_str_to_float(amount)[1])
     if not number_str_to_float(amount)[1]:
         raise ValidationError(f'{amount} is not a valid quantity format please try again.')
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from .forms import RecipeForm, RecipeIngredientFormset, RecipeStepFormset
@@ -33,46 +33,31 @@ class RecipeCreateView(CreateView, LoginRequiredMixin, UserPassesTestMixin):
     model = Recipe
     form_class = RecipeForm
     
-    def get(self, request):
-        super(RecipeCreateView, self).get(request)
-        self.context = self.get_context_data()
-        self.context['RecipeIngredientFormset'] = RecipeIngredientFormset()
-        self.context['RecipeStepFormset'] = RecipeStepFormset()
-        return render(request, 'recipes/recipe_form.html' , self.context)
-
-    def post(self, request):
-        RecipeForm = self.get_form()
-        if RecipeForm.is_valid():
-            return self.form_valid(RecipeForm)
+    def get_context_data(self, **kwargs):
+        context = super(RecipeCreateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['RecipeIngredientFormset'] = RecipeIngredientFormset(self.request.POST) 
+            context['RecipeStepFormset'] = RecipeStepFormset(self.request.POST)
         else:
-            self.object = None
-            return self.form_invalid(RecipeForm)
+            context['RecipeIngredientFormset'] = RecipeIngredientFormset()
+            context['RecipeStepFormset'] = RecipeStepFormset()
+        return context
 
     def form_valid(self, RecipeForm):
-        RecipeForm.instance.author = self.request.user
-        self.object = RecipeForm.save(commit=False)
-        IngredientFormset = RecipeIngredientFormset(self.request.POST, instance=self.object)
-        StepFormset = RecipeStepFormset(self.request.POST, instance=self.object)
+        context = self.get_context_data()
+        IngredientFormset = context['RecipeIngredientFormset']
+        StepFormset = context['RecipeStepFormset']
         if IngredientFormset.is_valid() and StepFormset.is_valid():
-            RecipeForm.save()
+            RecipeForm.instance.author = self.request.user
+            self.object = RecipeForm.save()
+            IngredientFormset.instance = self.object
             IngredientFormset.save()
+            StepFormset.instance = self.object
             StepFormset.save()
-            return redirect(self.object.get_absolute_url())
         else:
-            for error in IngredientFormset.errors:
+            for error in IngredientFormset.errors + StepFormset.errors:
                 messages.error(self.request, f'{error}')
-            for error in StepFormset.errors:
-                messages.error(self.request, f'{error}')
-            return self.form_invalid(RecipeForm)
-
-    def form_invalid(self, RecipeForm):
-        self.context = self.get_context_data()
-        self.context['RecipeForm'] = RecipeForm
-        self.context['RecipeIngredientFormset'] = RecipeIngredientFormset()
-        self.context['RecipeStepFormset'] = RecipeStepFormset()
-        for error in RecipeForm.errors:
-            messages.error(self.request, f'{error}')
-        return render(self.request, 'recipes/recipe_form.html', self.context)
+        return super(RecipeCreateView, self).form_invalid(RecipeForm)
 
 
 class RecipeUpdateView(UpdateView, LoginRequiredMixin):
