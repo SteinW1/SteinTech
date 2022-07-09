3f6ea52d2ac891f7043155c8a4fac34a70169f99
SteinW1 <will.stein@yahoo.com>
2022-07-09 00:22:09 -0700

    Added inline forms to RecipeUpdateView.

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 from .forms import RecipeForm, RecipeIngredientFormset, RecipeStepFormset
-from .models import Recipe, Ingredient
+from .models import Recipe
 
 
 def home(request):
@@ -17,11 +17,6 @@ def home(request):
 class RecipeDetailView(DetailView):
     model = Recipe
     context_object_name = "recipe"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(RecipeDetailView, self).get_context_data(*args, **kwargs)
-        context['ingredients'] = Ingredient.objects.all()
-        return context
 
 
 class RecipeListView(ListView):
@@ -54,16 +49,42 @@ class RecipeCreateView(CreateView, LoginRequiredMixin, UserPassesTestMixin):
             IngredientFormset.save()
             StepFormset.instance = self.object
             StepFormset.save()
+            return super(RecipeCreateView, self).form_valid(RecipeForm)
         else:
             for error in IngredientFormset.errors + StepFormset.errors:
                 messages.error(self.request, f'{error}')
-        return super(RecipeCreateView, self).form_invalid(RecipeForm)
+            return super(RecipeCreateView, self).form_invalid(RecipeForm)
 
 
 class RecipeUpdateView(UpdateView, LoginRequiredMixin):
     model = Recipe
     form_class = RecipeForm
     template_name_suffix = '_update_form'
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['RecipeIngredientFormset'] = RecipeIngredientFormset(self.request.POST, instance=self.object) 
+            context['RecipeStepFormset'] = RecipeStepFormset(self.request.POST, instance=self.object)
+
+        else:
+            context['RecipeIngredientFormset'] = RecipeIngredientFormset(instance=self.object) 
+            context['RecipeStepFormset'] = RecipeStepFormset(instance=self.object)
+        return context
+
+    def form_valid(self, RecipeForm):
+        context = self.get_context_data(form=RecipeForm)
+        RecipeIngredientFormset = context['RecipeIngredientFormset']
+        RecipeStepForm = context['RecipeStepFormset']
+        if RecipeIngredientFormset.is_valid() and RecipeStepForm.is_valid():
+            response = super().form_valid(RecipeForm)
+            RecipeIngredientFormset.instance = self.object
+            RecipeIngredientFormset.save()
+            RecipeStepForm.instance = self.object
+            RecipeStepForm.save()
+            return response
+        else:
+            return super().form_invalid(RecipeForm)
 
 
 class RecipeDeleteView(DeleteView, LoginRequiredMixin):
